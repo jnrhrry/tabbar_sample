caaaed995399c29588ca537e1084758900137a7d
Harry Januar <januarjanuarharry@gmail.com>
2020-09-22 16:37:46 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    TabBar myTabBar = TabBar(
+        indicatorColor: Colors.red,
+        indicator: BoxDecoration(
+            color: Colors.red,
+            border: Border(top: BorderSide(color: Colors.white, width: 5))),
+        tabs: <Widget>[
+          Tab(
+            icon: Icon(Icons.comment),
+            text: "Comments",
+          ),
+          Tab(
+            icon: Icon(Icons.computer),
+            text: "Computer",
+          ),
+        ]);
+    return MaterialApp(
+      home: DefaultTabController(
+          length: 2,
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text("Contoh Tab Bar"),
+              backgroundColor: Colors.amber,
+              bottom: PreferredSize(
+                  preferredSize: Size.fromHeight(myTabBar.preferredSize.height),
+                  child: Container(color: Colors.amber, child: myTabBar)),
+            ),
+            body: TabBarView(children: <Widget>[
+              Center(
+                child: Text("Tab 1"),
+              ),
+              Center(
+                child: Text("Tab 2"),
+              )
+            ]),
+          )),
+    );
+  }
+}
